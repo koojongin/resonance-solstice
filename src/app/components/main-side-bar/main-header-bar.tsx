90cba63576f12c9724fba58d406496ea32fc5538
koojongin <jiku90@naver.com>
2025-02-16 17:49:08 +0900

    feat: update favicon, update header direct links

--- src/app/components/main-side-bar/main-header-bar.tsx
+++ src/app/components/main-side-bar/main-header-bar.tsx
@@ -6,11 +6,30 @@ import Link from 'next/link'
 export default function MainHeaderBar() {
   const router = useRouter()
   return (
-    <Link href="https://arca.live/b/resonance">
-      <div className="flex items-center text-white gap-[10px] cursor-pointer">
-        <img className="w-[30px] rounded-full border-[2px]" src="/img/arca.svg" />
-        레조넌스 채널로 이동
-      </div>
-    </Link>
+    <div className="flex flex-wrap gap-[10px]">
+      <Link href="https://arca.live/b/resonance" target="_blank">
+        <div className="flex items-center text-white gap-[10px] cursor-pointer">
+          <img className="w-[30px] rounded-full border-[2px]" src="/img/arca.svg" />
+          레조넌스 채널
+        </div>
+      </Link>
+
+      <Link href="https://gall.dcinside.com/mgallery/board/lists/?id=reso" target="_blank">
+        <div className="flex items-center text-white gap-[10px] cursor-pointer">
+          <img className="w-[30px] rounded-full border-[2px]" src="/img/dc.ico" />
+          갤러리
+        </div>
+      </Link>
+
+      <Link href="https://game.naver.com/lounge/RES/home" target="_blank">
+        <div className="flex items-center text-white gap-[10px] cursor-pointer">
+          <img
+            className="w-[30px] rounded-full border-[2px] bg-white"
+            src="/img/naver-lounge.svg"
+          />
+          네이버 라운지
+        </div>
+      </Link>
+    </div>
   )
 }
