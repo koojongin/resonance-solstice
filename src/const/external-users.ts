d512cec951e5ad23de7adb521caf4b0f64f64009
koojongin <jiku90@naver.com>
2025-02-18 15:05:43 +0900

    feat: add new decks

--- src/const/external-users.ts
+++ src/const/external-users.ts
@@ -6,4 +6,5 @@ export interface ExternalUser {
 export const EXTERNAL_USERS = {
   1: { name: '사키르', link: 'https://arca.live/u/@%EC%82%AC%ED%82%A4%EB%A5%B4' },
   2: { name: 'lXHQyHoy39', link: 'https://arca.live/u/@lXHQyHoy39' },
+  3: { name: 'Burel', link: 'https://arca.live/u/@Burel/25353406' },
 }
