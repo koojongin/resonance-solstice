34d03b8997d8832df2ffb42c91d31bfd4ee45593
koojongin <jiku90@naver.com>
2025-02-22 23:51:30 +0900

    feat: add equip page

--- src/app/equipments/page.tsx
+++ src/app/equipments/page.tsx
@@ -1,55 +1,169 @@
 'use client'
 
 import React, { useState } from 'react'
-import _ from 'lodash'
 import createKey from '@/services/key-generator'
-import { useNextDepthNavigator } from '@/services/navigation'
-import { ALL_EQUIPMENTS, RSEquipment } from '@/const/archieve/equipment.const'
-import {
-  getMaterialBgFrameUrl,
-  getMaterialBottomFrameUrl,
-} from '@/services/utils/material-box-converter'
+import { CheckBoxGroup } from '@/services/common.enum'
+import { RS_FACTION, RS_GRADE } from '@/const/character/character.enum'
+import { RsEquipmentList } from '@/app/equipments/rs-equipment-list'
+import { RSEquipmentType } from '@/const/archieve/equipment.const'
 
-type ExtendedRSEquipment = RSEquipment & { name: string }
 export default function EquipmentsPage() {
-  const { router } = useNextDepthNavigator()
-  const [equipments, setEquipments] = useState<ExtendedRSEquipment[]>(
-    _.map(ALL_EQUIPMENTS, (value, key) => ({
-      name: key,
-      ...value,
-    })),
+  const [searchedKeyword, setSearchedKeyword] = useState('')
+  const [checkedGrades, setCheckedGrades] = useState<CheckBoxGroup>(
+    // Object.keys(RS_GRADE).reduce((acc, key) => ({ ...acc, [key]: key === RS_GRADE.SSR }), {}),
+    Object.keys(RS_GRADE).reduce(
+      (acc, key) => ({
+        ...acc,
+        [key]: true,
+      }),
+      {},
+    ),
   )
+
+  const [checkedEquipTypes, setCheckedEquipTypes] = useState<CheckBoxGroup>(
+    Object.keys(RSEquipmentType).reduce(
+      (acc, key) => ({
+        ...acc,
+        [key]: true,
+      }),
+      {},
+    ),
+  )
+
+  const [checkedFactions, setCheckedFactions] = useState<CheckBoxGroup>(
+    Object.keys(RS_FACTION).reduce(
+      (acc, key) => ({
+        ...acc,
+        [key]: true,
+      }),
+      {},
+    ),
+  )
+
+  const handleChangeGrade = (event: any) => {
+    const { name, checked } = event.target
+    setCheckedGrades((prev) => ({
+      ...prev,
+      [name]: checked,
+    }))
+  }
+
+  const handleChangeFaction = (event: any) => {
+    const { name, checked } = event.target
+    setCheckedFactions((prev) => ({
+      ...prev,
+      [name]: checked,
+    }))
+  }
+
+  const handleChangeEquipType = (event: any) => {
+    const { name, checked } = event.target
+    setCheckedEquipTypes((prev) => ({
+      ...prev,
+      [name]: checked,
+    }))
+  }
+
+  const handleSearchedKeywordChange = (event: any) => {
+    const { value } = event.target
+    setSearchedKeyword(value)
+  }
+
   return (
-    <div className="flex flex-col gap-[8px]">
-      {equipments.map((equipment) => {
-        return (
-          <div
-            key={createKey()}
-            className="flex gap-[4px] border items-stretch shadow-md cursor-pointer"
-            onClick={() => router.push(`/equipments/${equipment.name}`)}
-          >
-            <div className="flex items-center justify-center">
-              <EquipmentBox equipment={equipment} />
-            </div>
-            <div className="flex flex-col p-[8px] px-[4px] gap-[4px]">
-              <div className="ff-dh text-[16px] text-white text-shadow-outline">
-                {equipment.name}
-              </div>
-              <hr />
-              <div className="whitespace-pre-line leading-tight">{equipment.desc}</div>
-            </div>
+    <div className="flex flex-col gap-[10px]">
+      <div className="flex flex-col gap-[6px] items-start justify-start border p-[8px] rounded shadow border-gray-400">
+        <div className="flex justify-start">
+          <div className="w-[100px] flex justify-start">등급</div>
+          <div className="flex items-center gap-[4px]">
+            {Object.keys(RS_GRADE).map((grade) => {
+              return (
+                <label
+                  className="flex items-center cursor-pointer select-none px-[4px]"
+                  key={createKey()}
+                >
+                  <input
+                    type="checkbox"
+                    className="flex border-2 rounded-md border-blue-200"
+                    name={grade}
+                    checked={checkedGrades[grade]}
+                    onChange={handleChangeGrade}
+                  />
+                  <div className="ml-[2px]">{grade}</div>
+                </label>
+              )
+            })}
           </div>
-        )
-      })}
-    </div>
-  )
-}
-function EquipmentBox({ equipment }: { equipment: ExtendedRSEquipment }) {
-  return (
-    <div className="h-[80px] w-[80px] relative border-2">
-      <img className="absolute bottom-0 z-0" src={getMaterialBgFrameUrl(equipment.grade)} />
-      <img className="relative z-20 rounded w-full p-[4px]" src={equipment.thumbnail} />
-      <img className="absolute z-10 bottom-0" src={getMaterialBottomFrameUrl(equipment.grade)} />
+        </div>
+
+        <div className="flex justify-start">
+          <div className="w-[100px] flex justify-start">진영</div>
+          <div className="flex items-center gap-[4px]">
+            {Object.keys(RS_FACTION).map((faction) => {
+              return (
+                <label
+                  className="flex items-center cursor-pointer select-none px-[4px]"
+                  key={createKey()}
+                >
+                  <input
+                    type="checkbox"
+                    className="flex border-2 rounded-md border-blue-200"
+                    name={faction}
+                    checked={checkedFactions[faction]}
+                    onChange={handleChangeFaction}
+                  />
+                  <div className="ml-[2px]">{RS_FACTION[faction as keyof typeof RS_FACTION]}</div>
+                </label>
+              )
+            })}
+          </div>
+        </div>
+        <div className="flex justify-start">
+          <div className="w-[100px] flex justify-start">타입</div>
+          <div className="flex items-center gap-[4px]">
+            {Object.keys(RSEquipmentType).map((eType) => {
+              return (
+                <label
+                  className="flex items-center cursor-pointer select-none px-[4px]"
+                  key={createKey()}
+                >
+                  <input
+                    type="checkbox"
+                    className="flex border-2 rounded-md border-blue-200"
+                    name={eType}
+                    checked={checkedEquipTypes[eType]}
+                    onChange={handleChangeEquipType}
+                  />
+                  <div className="ml-[2px]">
+                    {RSEquipmentType[eType as keyof typeof RSEquipmentType]}
+                  </div>
+                </label>
+              )
+            })}
+          </div>
+        </div>
+
+        <div className="flex justify-start">
+          <div className="w-[100px] flex justify-start items-center">검색</div>
+          <div className="flex items-center gap-[4px] my-[5px] relative">
+            <input
+              className="border border-gray-400 min-w-[300px] p-[4px]"
+              type="text"
+              value={searchedKeyword}
+              onChange={handleSearchedKeywordChange}
+              placeholder="검색할 장비 이름을 입력하세요."
+            />
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <RsEquipmentList
+          checkedGrades={checkedGrades}
+          checkedFactions={checkedFactions}
+          searchedKeyword={searchedKeyword}
+          checkedEquipTypes={checkedEquipTypes}
+        />
+      </div>
     </div>
   )
 }
---
+++ src/app/equipments/rs-equipment-list.tsx
@@ -0,0 +1,101 @@
+'use client'
+
+import {
+  getMaterialBgFrameUrl,
+  getMaterialBottomFrameUrl,
+} from '@/services/utils/material-box-converter'
+import React, { useEffect, useState } from 'react'
+import createKey from '@/services/key-generator'
+import { useNextDepthNavigator } from '@/services/navigation'
+import { ALL_EQUIPMENTS, RSEquipment, RSEquipmentType } from '@/const/archieve/equipment.const'
+import _ from 'lodash'
+import { RS_FACTION } from '@/const/character/character.enum'
+
+const CONVERTED_ALL_EQUIPMENTS = _.map(ALL_EQUIPMENTS, (value, key) => ({
+  name: key,
+  ...value,
+}))
+
+export function RsEquipmentList({
+  checkedGrades,
+  checkedFactions,
+  searchedKeyword,
+  checkedEquipTypes,
+}: any) {
+  const { router } = useNextDepthNavigator()
+  const [equipments, setEquipments] = useState<ExtendedRSEquipment[]>(CONVERTED_ALL_EQUIPMENTS)
+
+  useEffect(() => {
+    setEquipments(
+      [...CONVERTED_ALL_EQUIPMENTS].filter((equipment) => {
+        const validGrades: string[] = _.keys(_.pickBy(checkedGrades, Boolean))
+        const isValidGrade = validGrades.includes(equipment.grade)
+
+        const validFactions: string[] = _.keys(_.pickBy(checkedFactions, Boolean)).map(
+          (key) => RS_FACTION[key as keyof typeof RS_FACTION],
+        )
+        const isValidFaction = validFactions.includes(equipment.faction)
+
+        const validEquipTypes: string[] = _.keys(_.pickBy(checkedEquipTypes, Boolean)).map(
+          (key) => RSEquipmentType[key as keyof typeof RSEquipmentType],
+        )
+        const isValidEquipType = validEquipTypes.includes(equipment.type)
+
+        const isValidKeyword = equipment.name.indexOf(searchedKeyword) >= 0
+        return (
+          isValidEquipType &&
+          isValidGrade &&
+          isValidFaction &&
+          (searchedKeyword ? isValidKeyword : true)
+        )
+      }),
+    )
+  }, [checkedEquipTypes, checkedFactions, checkedGrades, searchedKeyword])
+
+  return (
+    <div className="flex flex-col gap-[4px]">
+      <div className="flex items-stretch gap-[10px]">
+        <div className="rounded-[4px] inline-flex p-[4px] text-white bg-gray-700 border-white/50 border-dotted border">
+          장비 - {equipments.length} / {CONVERTED_ALL_EQUIPMENTS.length}
+        </div>
+        {searchedKeyword && (
+          <div className="rounded-[4px] flex items-center">"{searchedKeyword}" 검색됨</div>
+        )}
+      </div>
+      <div className="flex flex-col gap-[8px]">
+        {equipments.map((equipment) => {
+          return (
+            <div
+              key={createKey()}
+              className="flex gap-[4px] border items-stretch shadow-md cursor-pointer"
+              onClick={() => router.push(`/equipments/${equipment.name}`)}
+            >
+              <div className="flex items-center justify-center">
+                <EquipmentBox equipment={equipment} />
+              </div>
+              <div className="flex flex-col p-[8px] px-[4px] gap-[4px]">
+                <div className="ff-dh text-[16px] text-white text-shadow-outline">
+                  {equipment.name}
+                </div>
+                <hr />
+                <div className="whitespace-pre-line leading-tight">{equipment.desc}</div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+type ExtendedRSEquipment = RSEquipment & { name: string }
+
+function EquipmentBox({ equipment }: { equipment: ExtendedRSEquipment }) {
+  return (
+    <div className="h-[80px] w-[80px] relative border-2">
+      <img className="absolute bottom-0 z-0" src={getMaterialBgFrameUrl(equipment.grade)} />
+      <img className="relative z-20 rounded w-full p-[4px]" src={equipment.thumbnail} />
+      <img className="absolute z-10 bottom-0" src={getMaterialBottomFrameUrl(equipment.grade)} />
+    </div>
+  )
+}
