95500657d03a7ee6ef8f6cf02ed858a032fa4857
koojongin <jiku90@naver.com>
2025-03-08 13:52:54 +0900

    feat: add desc

--- src/app/rd/eternal-scuffle/page.tsx
+++ src/app/rd/eternal-scuffle/page.tsx
@@ -33,7 +33,8 @@ import { RdEternalScuffleList } from '@/app/rd/eternal-scuffle/rd-eternal-scuffl
 import { debounce } from 'lodash'
 
 export default function RdEternalScufflePage() {
-  const [isVisibleGuideBox, setIsVisibleGuideBox] = useState(false)
+  const [isVisibleChoiceGuideBox, setIsVisibleChoiceGuideBox] = useState(false)
+  const [isVisibleInfoBox, setIsVisibleInfoBox] = useState(false)
   const [searchedKeyword, setSearchedKeyword] = useState('')
 
   const handleSearchedKeywordChange = debounce((event: any) => {
@@ -45,38 +46,70 @@ export default function RdEternalScufflePage() {
       <div className="flex flex-col gap-[10px]">
         <div className="flex flex-col gap-[4px]">
           <div className="text-[30px] ff-dh">무한 난투</div>
-          <div className="flex flex-col gap-[4px]">
-            <div>
-              * 캐릭터 선택시 강제 종료하면 재선택이 가능합니다.(재선택은 가능하지만 원래 고르던
-              선택지는 변화 없음)
-            </div>
-            <div>* 전투가 종료되기 전 강제 종료하면 라이프 소모 없이 재도전이 가능합니다.</div>
-            <div>
-              * 무한난투는 하루 최대 4회 가능합니다. 추가 기회는 300,000 [철도연맹 코인]이
-              소모됩니다.
-            </div>
-            <div>
-              8단계 까지 모두 클리어시 약 66만원+[랜덤 장비 및 교환 상점 코인]를 획득합니다.
-            </div>
+          <div
+            className="ff-dh text-[24px] flex gap-[4px] items-center cursor-pointer select-none"
+            onClick={() => setIsVisibleInfoBox((prev) => !prev)}
+          >
+            일반 정보
+            {isVisibleInfoBox && <i className="fa-solid fa-square-minus" />}
+            {!isVisibleInfoBox && <i className="fa-solid fa-square-caret-down" />}
           </div>
+          <motion.div
+            initial={{ height: 0 }}
+            animate={{ height: isVisibleInfoBox ? 'auto' : 0 }}
+            transition={{
+              type: 'tween',
+              ease: 'easeOut',
+              duration: 0.1,
+            }}
+            className={`${isVisibleInfoBox ? '' : 'overflow-hidden'}`}
+          >
+            <div className="flex flex-col gap-[8px] border border-gray-700 p-[8px] rounded">
+              <div>
+                * 캐릭터 선택 도중{' '}
+                <span className="text-red-500 underline">강제 종료하면 재선택</span>이
+                가능합니다.(재선택은 가능하지만 원래 고르던 선택지는 변화 없음)
+              </div>
+              <div>
+                * 전투 도중{' '}
+                <span className="text-red-500 underline">
+                  강제 종료하면 라이프 소모 없이 재도전
+                </span>
+                이 가능합니다.
+              </div>
+              <div>
+                * 무한난투는 하루 최대 4회 가능합니다. 추가 기회 1번당 300,000 [철도연맹 코인]이
+                소모됩니다.
+              </div>
+              <div className="whitespace-pre-line leading-tight">
+                * 클리어시 [철도연맹 코인],[랜덤 장비],[교환 상점 코인]을 랜덤하게 획득합니다.
+                <br />
+                <span className="font-bold">※ 각 단계 당 상자 1개</span>
+                <br />* 보상 결과에서 [철도연맹 코인]이 걸릴 확률이 가장 높은듯 8단계까지가면 보통
+                30만원짜리 두개 정도 나오는 것같음
+              </div>
+              <div className="ff-dh text-[20px] mt-[15px]">예시 클리어 결과 이미지</div>
+              <img src="https://nng-phinf.pstatic.net/MjAyNTAzMDdfMTAz/MDAxNzQxMzE3MTgwNDM4.Bq-gFzyggZQVHFy1SxXIfb7Albch9JoyhgfMktO21KMg._Gu7TThTJLMAfuT9Dz81lmPFM3yvbcJc0cwPYYUV0GMg.JPEG/KakaoTalk_20250307_120800107_01.jpg?type=w1678" />
+            </div>
+          </motion.div>
         </div>
         <div
           className="ff-dh text-[24px] flex gap-[4px] items-center cursor-pointer select-none"
-          onClick={() => setIsVisibleGuideBox((prev) => !prev)}
+          onClick={() => setIsVisibleChoiceGuideBox((prev) => !prev)}
         >
           잘 모를때 선택 가이드
-          {isVisibleGuideBox && <i className="fa-solid fa-square-minus" />}
-          {!isVisibleGuideBox && <i className="fa-solid fa-square-caret-down" />}
+          {isVisibleChoiceGuideBox && <i className="fa-solid fa-square-minus" />}
+          {!isVisibleChoiceGuideBox && <i className="fa-solid fa-square-caret-down" />}
         </div>
         <motion.div
           initial={{ height: 0 }}
-          animate={{ height: isVisibleGuideBox ? 'auto' : 0 }}
+          animate={{ height: isVisibleChoiceGuideBox ? 'auto' : 0 }}
           transition={{
             type: 'tween',
             ease: 'easeOut',
             duration: 0.1,
           }}
-          className={`${isVisibleGuideBox ? '' : 'overflow-hidden'}`}
+          className={`${isVisibleChoiceGuideBox ? '' : 'overflow-hidden'}`}
         >
           <div className="flex flex-col gap-[8px] border border-gray-700 p-[8px] rounded">
             <RSHighlightedText
